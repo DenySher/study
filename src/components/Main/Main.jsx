8d6c15be41549261c8e91b35cc8ec8b4c9ee4c64
DenySher <d15m11@list.ru>
2022-06-16 22:41:16 +0300

    Modal

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,22 @@
-import Component1 from "../component1/Component1"
 import Modal from "../Modal/Modal"
+import { useState } from "react"
+
 
 const Main = () => {
+
+    const [modal, setModal] = useState(false)
+    const Toggle = () => setModal(!modal)  
+
     return (
-        <>
-        <Component1 />
-        <Modal />
-        </>
+        <div>
+            <button onClick={Toggle}>Modal
+                {/* {modal ? 'open' : 'close'} */}
+            </button>
+            <Modal modal={modal} close={Toggle}/>
+        </div>
     )
 }
+
+//https://dev.to/codebucks/how-to-create-an-efficient-modal-component-in-react-using-hooks-and-portals-360p
 
 export default Main
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,26 @@
-const Modal = () => {
-    return (
-        <>
-        <div>
-            <button>HI</button>
+import styles from './Modal.module.scss'
+
+
+const  Modal = ({ modal, close }) => {
+  
+
+  return (
+        
+        <div className={styles.containerModal} onClick={close}>
+            {
+                modal ?
+            <div>
+                <p>HI</p>
+                <button onClick={close}>Close</button>
+            </div>
+
+            : null
+            }
+
         </div>
-        </>
-    )
+        
+  )
 }
+
 
 export default Modal
