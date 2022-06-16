f2233f59a68488f12b6bb6aa733c8ed48f6d522b
DenySher <d15m11@list.ru>
2022-06-16 12:56:04 +0300

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import styles from './index.module.scss'
+import Main from './components/Main/Main'
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
